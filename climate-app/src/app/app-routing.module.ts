ee2f4ca333350865147bed037537aaf07407ffd7
Wisdom John ikoi <wisdomjohnikoi@gmail.com>
2022-12-14 17:48:32 +0100

    tailwind-install

--- climate-app/src/app/app-routing.module.ts
+++ climate-app/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ClimateRegionComponent } from './climate-region/climate-region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'regions', component: ClimateRegionComponent},
+  // {path: 'login', component: LoginComponent},
+  // {path: 'register', component: RegisterComponent},
+  // {path: 'forgot-password', component: ForgotPasswordComponent},
+  // {path: 'verify-email', component: VerifyEmailComponent},
+  // {path: 'dashboard', component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
+
 export class AppRoutingModule {}
---
+++ climate-app/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+ };
